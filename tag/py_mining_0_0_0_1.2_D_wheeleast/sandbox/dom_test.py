846a08f8a40fe4ceda995ad2c4e362b96ac36a98
wheeleast@gmail.com <wheeleast@gmail.com@5e8d6943-abb3-2f53-c6ab-480f8ed250c6>
2012-02-08 03:10:45 +0000

    mv tags to right position

--- tag/py_mining_0_0_0_1.2_D_wheeleast/sandbox/dom_test.py
+++
@@ -1,8 +0,0 @@
-from xml.dom import minidom
-
-pxml = minidom.parse("test.xml")
-print pxml.getElementsByTagName("cfg")
-pnode = pxml.firstChild
-for node in pnode.getElementsByTagName("a"):
-    print node.firstChild.data
-
